88683b13bc4c12f0a160445181a4297398de2925
rasa9521 <rsrasasingh@gmail.com>
2025-04-14 22:22:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ bot = Client(
     "bot",
     api_id=27900743,
     api_hash=ebb06ea8d41420e60b29140dcee902fc,
-    bot_token=8003649544:AAGoiThVN8KLJyKGsGf1BcfTsjDTrSmjFR8
+    bot_token=BOT_TOKEN
 )
 
 
